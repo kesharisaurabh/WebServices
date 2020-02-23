be7ecbd79835a98fe2f526fcc436a1f40050a06c
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-24 00:33:28 +0530

    Add files via upload

---
+++ WebApp_04_Mock_resource_xml/src/main/java/sk/WebApp_02/stdResource.java
@@ -0,0 +1,28 @@
+package sk.WebApp_02;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("stdRes")
+public class stdResource 
+{
+	studentRepositry strepo=new studentRepositry();
+	
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public List<student> getStudent()
+	{
+		
+		
+		System.out.println("GetStudent");
+		return strepo.getStudent();
+		
+	}
+}
