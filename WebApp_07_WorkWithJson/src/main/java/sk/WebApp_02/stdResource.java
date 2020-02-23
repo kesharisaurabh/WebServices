373851357efe1cfd015274f45beeaf9ab328772a
SAURABH KESHARI <keshari.saurabh@yahoo.in>
2020-02-24 01:01:27 +0530

    Add files via upload

---
+++ WebApp_07_WorkWithJson/src/main/java/sk/WebApp_02/stdResource.java
@@ -0,0 +1,52 @@
+package sk.WebApp_02;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.server.model.ParamQualifier;
+
+@Path("stdRes")
+public class stdResource 
+{
+	studentRepositry strepo=new studentRepositry();
+	
+	@GET
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public List<student> getStudent()
+	{
+		
+		
+		System.out.println("GetStudent");
+		return strepo.getStudent();
+		
+	}
+	
+	@POST
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	@Path("AddStudent")
+	public student setStudent(student std)
+	{
+		System.out.println("Student Created");
+		
+		strepo.create(std);
+		return std;
+		
+	}
+	
+	@GET
+	@Path("getStud/{id}")
+	public student setStudent(@PathParam("id")int id)
+	{
+		System.out.println("Student getByID");
+		return strepo.getStu(id);
+	}
+}
